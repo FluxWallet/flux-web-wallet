68a1cf6dc904e60cb99aa92409e6f0b87b85b05d
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2023-04-11 04:05:16 +0530

    Remove unused dataContext
    The `dataContext` was identified as an unused React Context. This commit removes its definition from `packages/app/src/types/index.ts` and all references and usage from `packages/app/src/pages/index.tsx`.
    
    This simplifies the component tree and removes unnecessary boilerplate.

--- packages/app/src/types/index.ts
+++ packages/app/src/types/index.ts
@@ -1,18 +1,7 @@
-import { createContext, useContext } from "react";
-
 export interface PeerMeta {
   name: string;
   url: string;
 }
-
-export interface GlobalContent {
-  uri: string;
-  address: string | undefined;
-}
-
-export const dataContext = createContext<GlobalContent>({ uri: "", address: "" });
-
-export const useDataContext = () => useContext(dataContext);
 
 export interface Deployments {
   entryPoint: string;
