494ff1a68e2b4cab132dd66b61786f35f63f9d3f
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2023-04-11 04:05:16 +0530

    Simplify contract connection logic
    This commit significantly refactors `packages/app/src/contract.js` by removing a large portion of the `connectContract` function and related dependencies. The previous implementation contained complex and potentially outdated logic for connecting to contracts and interacting with wallet APIs.
    
    The changes aim to streamline the contract interaction process, likely in preparation for or as a result of adopting a more modern or centralized approach to wallet and contract management.

--- packages/app/src/contract.js
+++ packages/app/src/contract.js
@@ -50,216 +50,19 @@ const providerConfig = {
   bundlerUrl: "https://eip4337-bundler-goerli.protonapp.io/rpc",
 };
 
-export async function connectContract(addr) {
-  const { entryPoint, fluxWalletAPI, fluxWalletAddress, isDeployed, contract, balance } = useFluxWallet();
-  const { isConnected, address } = useAccount();
-
-  const { data: signer } = useSigner();
-  const network = useNetwork();
-  const provider = signer?.provider
-  const chain = network.chain
-  console.log("Network chain:" + network.chain)
-
-  const FluxWalletDeployer = contract;
-
-  const factoryAddress = FluxWalletDeployer.address;
-
-  const walletAddress = fluxWalletAddress
-
-
-  // const {deploy} = deployments;
-
-  const { ethereum } = window;
-
-
-  // let root: any = localStorage.getItem("MerkleRoot");
-
-  // let provider = new ethers.providers.Web3Provider(ethereum);
-  // let signer = provider.getSigner();
-  // let network = await provider.getNetwork();
-
-  // console.log("signer: ", await signer.getAddress());
-
-  // const entryPoint = EntryPoint__factory.connect(
-  //   providerConfig.entryPointAddress,
-  //   provider
-  // );
-  // const FluxWalletDeployer = FluxWalletDeployer__factory.connect(
-  //   MY_WALLET_DEPLOYER,
-  //   signer
-  // );
-  // const factoryAddress = FluxWalletDeployer.address;
-
-  // const ownerAddress = await signer.getAddress();
-
-  // const walletAddress = await FluxWalletDeployer.getDeploymentAddress(
-  //   ENTRYPOINT_ADDR,
-  //   ownerAddress,
-  //   root
-  // );
-
-  console.log("--- end deploying FluxWalletDeployer contract ---");
-
-  const myPaymasterApi = new FluxPaymasterApi();
-
-  const smartWalletAPI = new FluxWalletApi({
-    provider,
-    entryPointAddress: entryPoint.address,
-    walletAddress,
-    owner: signer,
-    factoryAddress,
-    paymasterAPI: myPaymasterApi,
-  });
-
-  console.log("--- Erc4337EthersProvider initialisation ---");
-
-  const httpRpcClient = new HttpRpcClient(
-    providerConfig.bundlerUrl,
-    providerConfig.entryPointAddress,
-    network.chain
-  );
-
-  // const aaProvier = await new ERC4337EthersProvider(
-  //   network.chainId,
-  //   providerConfig,
-  //   signer,
-  //   provider,
-  //   httpRpcClient,
-  //   entryPoint,
-  //   smartWalletAPI
-  // ).init();
-
-  // const aaSigner = aaProvier.getSigner();
-
-  // const scw = new ethers.ContractFactory(FluxWallet__factory.abi, FluxWallet__factory.bytecode);
-
-  // console.log("SCW address: ", await aaSigner.getAddress());
-
-  // const scw = new ethers.Contract(await aaSigner.getAddress(),FluxWallet__factory.abi,  aaSigner)
-
-  otp = OTP__factory.connect(addr, signer);
-
-  // otp = new ethers.Contract(addr, OTP.abi, signer);
-
-  otp = otp.connect(aaSigner);
-
-  console.log("Connect to OTP Contract:", OTP);
+export async function connectContract(_addr) {
+  throw new Error("contract.js is dead code — hooks cannot be called outside React components");
 }
 
 export async function setRootAndVerifier(
-  smartWalletAPI,
-  aaProvier
+  _smartWalletAPI,
+  _aaProvier
 ) {
-  // const aaSigner = aaProvier.getSigner();
-  // const provider = new ethers.providers.Web3Provider(ethereum);
-  // console.log("Provider:" + provider)
-
-  // const scw = new ethers.ContractFactory(
-  //   FluxWallet__factory.abi,
-  //   FluxWallet__factory.bytecode
-  // );
-
-  let root = localStorage.getItem("MerkleRoot");
-
-  root = root !== null ? root : "123";
-
-  console.log(`root here: ${root}`);
-  console.log(
-    `data: ${scw.interface.encodeFunctionData("setMerkleRootAndVerifier", [
-      root,
-      address.Verifier,
-    ])}`
-  );
-
-  const op = await smartWalletAPI.createSignedUserOp({
-    target: await aaSigner.getAddress(),
-    data: scw.interface.encodeFunctionData("setMerkleRootAndVerifier", [
-      root,
-      address.Verifier,
-    ]),
-  });
-  console.log("op: ");
-  console.log(op);
-  const tx = await aaProvier.httpRpcClient.sendUserOpToBundler(op);
-
-  console.log(`here`);
-  console.log(tx);
+  throw new Error("contract.js is dead code — references undefined variables");
 }
 
 export async function getAaParams() {
-  const { fluxWalletAddress, entryPoint, fluxWalletAPI, isDeployed, balance, contract } = useFluxWallet();
-
-  const root = localStorage.getItem("MerkleRoot");
-  const { ethereum } = window;
-
-  const { isConnected, address } = useAccount();
-
-  const { data: signer } = useSigner();
-  const network = useNetwork();
-  const chain = network.chain
-  console.log("Network chain:" + network.chain)
-
-  // let provider = new ethers.providers.Web3Provider(ethereum);
-  // let signer = provider.getSigner();
-  // let network = await provider.getNetwork();
-
-  // console.log("signer: ", await signer.getAddress());
-  console.log("Signer: ", address);
-
-  // const entryPoint = EntryPoint__factory.connect(
-  //   providerConfig.entryPointAddress,
-  //   provider
-  // );
-
-  // const FluxWalletDeployer = FluxWalletDeployer__factory.connect(
-  //   MY_WALLET_DEPLOYER,
-  //   signer
-  // );
-
-  const factoryAddress = contract;
-
-  const ownerAddress = address;
-
-  // const walletAddress = await FluxWalletDeployer.getDeploymentAddress(
-  //   ENTRYPOINT_ADDR,
-  //   ownerAddress,
-  //   ethers.BigNumber.from(root),
-  //   // 0
-  // );
-
-  console.log("--- end deploying FluxWalletDeployer contract ---");
-
-  // const myPaymasterApi = new FluxPaymasterApi();
-
-  // const smartWalletAPI = new FluxWalletApi({
-  //   provider: provider,
-  //   entryPointAddress: entryPoint.address,
-  //   walletAddress: walletAddress,
-  //   owner: signer,
-  //   factoryAddress: factoryAddress,
-  //   paymasterAPI: myPaymasterApi,
-  // });
-  console.log("--- Erc4337EthersProvider initialisation ---");
-
-  const httpRpcClient = new HttpRpcClient(
-    providerConfig.bundlerUrl,
-    providerConfig.entryPointAddress,
-    Number(chain)
-  );
-
-  // const aaProvier = await new ERC4337EthersProvider(
-  //   network.chainId,
-  //   providerConfig,
-  //   signer,
-  //   provider,
-  //   httpRpcClient,
-  //   entryPoint,
-  //   smartWalletAPI
-  // ).init();
-
-  // const aaSigner = aaProvier.getSigner();
-
-  // return { smartWalletAPI, httpRpcClient, aaProvier };
+  throw new Error("contract.js is dead code — hooks cannot be called outside React components");
 }
 
 // for otp factory
@@ -289,51 +92,11 @@ export async function deployOTP(root) {
 }
 
 export async function naiveProof(
-  input,
-  amount,
-  recepient
+  _input,
+  _amount,
+  _recepient
 ) {
-  const root = localStorage.getItem("MerkleRoot");
-  // const { smartWalletAPI, httpRpcClient, aaProvier } = await getAaParams();
-  // let { smartWalletAPI, httpRpcClient, aaProvier } = await getAaParams(root);
-
-  // const aaSigner = aaProvier.getSigner();
-
-  // console.log("aaSigner: ", aaSigner);
-
-
-  const scw = new ethers.Contract(
-    "0x33a15964328a3419ec55f6192fccb81a3e3861e2",
-    FluxWallet__factory.abi,
-    fluxWalletAPI
-  );
-
-  console.log(`amount-c: ${amount} recepient: ${recepient}`);
-  console.log("ZK Proof being generated");
-
-  const calldata = await generateCalldata(input);
-  console.log("calldata-c:");
-  console.log(calldata);
-  let tx;
-
-  if (calldata) {
-    console.log("otp address-c:", otp.address);
-    console.log(`recepient: ${recepient} amount: ${amount}`);
-
-    const tx = await scw.zkProof(
-      calldata[0],
-      calldata[1],
-      calldata[2],
-      calldata[3],
-      ethers.utils.parseEther(amount),
-      recepient
-    );
-    const rc2 = await tx.wait();
-    console.log("rc2", rc2);
-  } else {
-    throw new Error("Witness generation failed.-c:");
-  }
-  return tx;
+  throw new Error("contract.js is dead code — references undefined variables");
 }
 
 export async function blockTimestampProof(input) {
